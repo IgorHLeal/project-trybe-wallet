56768d0e77fb75fff05e75b4b4f07e9753ea0f6e
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-31 20:14:56 -0300

    feat: Requisito 2 concluído

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Wallet from './pages/Wallet';
 import Login from './pages/Login';
 
 function App() {
   return (
     <Switch>
-      <Route extac path="/" component={ Login } />
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/carteira" component={ Wallet } />
     </Switch>
   );
 }
