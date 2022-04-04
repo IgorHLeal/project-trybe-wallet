d2c1d496cc379b4b62c551e194da06ef4eb6de5e
Igor Leal <igorhenriqueleal3@gmail.com>
2022-04-04 15:40:37 -0300

    refactor: Reestruturação e realocação de páginas e componentes para melhor desenvolvimento do requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,16 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchCurrencyQuotation } from '../actions';
-import '../styles/wallet.css';
+import WalletHeader from '../components/WalletHeader';
 
 class Wallet extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-    };
-  }
-
   componentDidMount() {
     // Requisição da API
     /* console.log('PROPS', this.props); */
@@ -21,29 +14,11 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { email } = this.props;
-    const { total } = this.state;
     return (
-      <div className="header-wallet">
-        {/* exiba o email da pessoa usuária que fez login */}
-        <div data-testid="email-field">{email}</div>
-
-        {/* Um elemento com a despesa total gerada pela lista de gastos. */}
-        <div data-testid="total-field">
-          Despesa total:
-          {total}
-        </div>
-
-        {/* Um elemento que mostre qual câmbio está sendo utilizado */}
-        <div data-testid="header-currency-field">BRL</div>
-      </div>
+      <WalletHeader />
     );
   }
 }
-
-const mapStateToProps = (state) => ({
-  email: state.user.email,
-});
 
 const mapDispatchToProps = (dispatch) => ({
   // Criar uma prop; Mesmo nome da função usada na requisição da api para manter um padrão;
@@ -52,8 +27,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 Wallet.propTypes = {
-  email: PropTypes.string.isRequired,
   getCurrencyQuotation: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
+export default connect(null, mapDispatchToProps)(Wallet);
